39de56c20a8790cb53440c89ca198ff417f164c2
Elmar Hinz <t3elmar@googlemail.com>
2013-03-15 17:17:13 +0100

    first steps with main action

---
+++ Configuration/Ext.php
@@ -0,0 +1,22 @@
+<?php
+
+class Ext {
+
+	static public function main($argv) {
+		$mb = __DIR__.'/../..';
+		require_once($mb.'/Cool/Classes/AutoLoader.php');
+		$loader = new \Cool\AutoLoader();
+		$loader->addModuleBase($mb);
+		$loader->go();
+		$loader = new \Cool\DedicatedDirectoriesLoader();
+		$loader->addModuleBase($mb);
+		$loader->go();
+		$container = new \Cool\Container();
+		$action = $container->getInstance('Ext\MainAction');
+		array_shift($argv);
+		$action->setCommands($argv);
+		$action->go();
+	}
+}
+
+?>
